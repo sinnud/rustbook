bd08b93bfbc93a38770969d3dcd36af768cb6347
Luke Du <sinnud@gmail.com>
2020-11-29 10:07:47 -0800

    chap5

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 //mod chap2;
-mod chap4;
+// mod chap4;
+mod chap5;
 fn main() {
     //chap2::main();
-    chap4::main11();
-    chap4::main12();
-    chap4::main13();
-    chap4::main21();
-    chap4::main22();
-    chap4::main31();
+    // chap4::main11();
+    // chap4::main12();
+    // chap4::main13();
+    // chap4::main21();
+    // chap4::main22();
+    // chap4::main31();
+    chap5::main21();
+    chap5::main22();
+    chap5::main23();
+    chap5::main24();
+    chap5::main31();
+    chap5::main32();
+    chap5::main33();
 }

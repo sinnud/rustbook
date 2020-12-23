b5486f588a13d99284bb0908c8f67c549d3eb1f8
Luke L Du <sinnud@gmail.com>
2020-12-23 13:31:33 -0800

    before move to postgresql

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+extern crate reqwest;
+/*
+pub async fn url2html(url: &str) -> &str {
+  let res = reqwest::get(url)
+  .await?
+  .text()
+  .await?
+  ;
+  res.as_str()
+}
+*/
+#[cfg(test)]
+mod tests {
+    use super::*;
+}

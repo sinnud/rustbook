9488d714d2d7cd1db0290e56448d359451a5e598
Luke Du <sinnud@gmail.com>
2021-01-01 20:06:47 -0800

    release with command line arguments

---
+++ src/envargs.rs
@@ -0,0 +1,26 @@
+pub fn envargs() ->Result<String, &'static str> {
+    // check command line argument
+    // no argument is default, do all works: data, music, photos, movie
+    // can be arbitrary argument(s) from the above four
+    // If duplicated, choose one
+    // if other than above four, error out
+    // return clean string
+    let mut args: Vec<String> = std::env::args().collect();
+    let default_opt="data music photos movie".to_owned();
+    if args.len() == 1{
+        return Ok(default_opt)
+    }
+    let mut res="".to_owned();
+    args.drain(0..1); // remove first one: execute program name
+    for e in args{
+        let good = e.to_lowercase();
+        if default_opt.contains(&good){
+            if res.len()==0{res.push_str(&good);}
+            else if ! res.contains(&good){
+                res.push_str(" ");
+                res.push_str(&good);
+            }
+        }
+    }
+    Ok(res)
+}
--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,35 @@ fn main()-> Result<(), &'static str>{
     
     lib_wd::file_status::rename_log_with_timestamp(&logfile)?;
     log4rs::init_file(&log4rs_yaml, Default::default()).unwrap();
+
+    let opt=lib_wd::envargs::envargs()?;
+    println!("Work on {}...", opt);
     
     let mut wd=lib_wd::wdinfo::WDInfo::default();
 
-    wd.wdrefresh("/mnt/music", "wdinfo", "music243")?;
-    wd.wdrefresh("/mnt/public/music", "wdinfo", "music241")?;
-    wd.wdsync("wdinfo", "music243", "music241")?;
+    if opt.contains("music"){
+        wd.wdrefresh("/mnt/music", "wdinfo", "music243")?;
+        wd.wdrefresh("/mnt/public/music", "wdinfo", "music241")?;
+        wd.wdsync("wdinfo", "music243", "music241")?;
+    }
 
-    wd.wdrefresh("/mnt/data", "wdinfo", "data243")?;
-    wd.wdrefresh("/mnt/public/data", "wdinfo", "data241")?;
-    wd.wdsync("wdinfo", "data243", "data241")?;
+    if opt.contains("data"){
+        wd.wdrefresh("/mnt/data", "wdinfo", "data243")?;
+        wd.wdrefresh("/mnt/public/data", "wdinfo", "data241")?;
+        wd.wdsync("wdinfo", "data243", "data241")?;
+    }
 
-    wd.wdrefresh("/mnt/photos", "wdinfo", "photos243")?;
-    wd.wdrefresh("/mnt/public/photos", "wdinfo", "photos241")?;
-    wd.wdsync("wdinfo", "photos243", "photos241")?;
+    if opt.contains("photos"){
+        wd.wdrefresh("/mnt/photos", "wdinfo", "photos243")?;
+        wd.wdrefresh("/mnt/public/photos", "wdinfo", "photos241")?;
+        wd.wdsync("wdinfo", "photos243", "photos241")?;
+    }
 
-    wd.wdrefresh("/mnt/movie", "wdinfo", "movie243")?;
+    if opt.contains("movie"){
+        wd.wdrefresh("/mnt/movie", "wdinfo", "movie243")?;
 
-    wd.wdrefresh("/mnt/public/newmovies", "wdinfo", "movie241")?;
+        wd.wdrefresh("/mnt/public/newmovies", "wdinfo", "movie241")?;
+    }
     
     Ok(())
 }
